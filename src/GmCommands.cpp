8891d5619e34a6c5804b25d756e4fa53239e9a4c
Andrew <47818697+Nyeriah@users.noreply.github.com>
2024-11-06 06:08:26 -0300

    Update GmCommands.cpp

--- src/GmCommands.cpp
+++ src/GmCommands.cpp
@@ -2,21 +2,7 @@
 #include "ScriptMgr.h"
 #include "Chat.h"
 #include "Config.h"
-class GmCommands : public PlayerScript
-{
-public:
-    GmCommands() : PlayerScript("GmCommands") {}
-
-    void OnLogin(Player* player) override
-    {
-        if (sConfigMgr->GetOption<bool>("GmCommandsModule.Enable", false))
-        {
-            ChatHandler(player->GetSession()).PSendSysMessage("Welcome, Game Master!");
-        }
-    }
-};
 
 void AddGmCommandScripts()
 {
-    new GmCommands();
 }
