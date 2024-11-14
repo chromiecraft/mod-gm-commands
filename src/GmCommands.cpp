2def980ccfd82328d2194c84370b59e19b9e3ec6
heyitsbench <benjymansy123@gmail.com>
2024-11-14 15:13:51 -0500

    Init.

--- src/GmCommands.cpp
+++ src/GmCommands.cpp
@@ -1,8 +1,47 @@
 #include "Player.h"
 #include "ScriptMgr.h"
 #include "Chat.h"
+#include "ChatCommand.h"
 #include "Config.h"
 
+class GmCommands : public AllCommandScript
+{
+public:
+    GmCommands() : AllCommandScript("GmCommands") {}
+
+    bool OnBeforeIsInvokerVisible(std::string name, Acore::Impl::ChatCommands::CommandPermissions permissions, ChatHandler const& who) override
+    {
+        if (who.IsConsole())
+            return true;
+
+        Player* player = who.GetPlayer();
+        
+        if (!player)
+            return true;
+
+        uint32 accountID = player->GetSession()->GetAccountId();
+
+        if (!std::count(eligibleAccounts.begin(), eligibleAccounts.end(), accountID))
+            return true; // Account is not eligible
+
+        if (std::count(eligibleCommands.begin(), eligibleCommands.end(), name))
+            return false; // Command is eligible
+
+        return true; // Command is not eligible
+    }
+
+private:
+    std::vector<uint32> eligibleAccounts =
+    {
+        94143, // HEYITSGMCH
+    };
+
+    std::vector<std::string> eligibleCommands =
+    {
+        "gm fly",
+    };
+};
 void AddGmCommandScripts()
 {
+    new GmCommands();
 }
